734ee2d5efd456bc47abe2f9966958c1950295ca
Ash Davies <ash.davies@outlook.com>
2016-08-28 11:01:22 +0100

    updated prod rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 set :stage, :production
 set :stage_url, "https://mineapple.co.uk"
 server "188.166.170.87", user: "ash", roles: %w{web app db}
-set :deploy_to, "/var/www/html/mineapple.co.uk"
+set :deploy_to, "/var/www/html/mineapple.co.uk/current"
 
 ############################################
 # Setup Git
